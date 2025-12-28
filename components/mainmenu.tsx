5434515e1dcf195d9ad8b33e6a10aeaf0ff5bcd3
Jean Felisme <jean@clockwork.com>
2025-12-28 01:54:21 -0500

    feat: update MainMenu component with new logo and improved navigation label

--- components/mainmenu.tsx
+++ components/mainmenu.tsx
@@ -8,7 +8,7 @@ interface NavItem {
 }
 
 const navItems: NavItem[] = [
-  { href: '/dashboard/generate-book', label: 'Generate' },
+  { href: '/dashboard/generate-book', label: 'Create a Book' },
   { href: '/login', label: 'Login' },
   { href: '/search', label: 'Search' },
 ];
@@ -18,8 +18,10 @@ export default function MainMenu(): React.JSX.Element {
     <nav className="flex flex-wrap items-center justify-between gap-2 border-b-2 border-purple-700 pb-2 text-lg text-purple-800">
       {/* Logo and title */}
       <Link href="/" className="mx-2 mt-2 flex items-center">
-        <Image src="/logo.svg" alt="logo" width={50} height={50} />
-        <span className="ml-2 text-xl font-bold text-purple-800">ai</span>
+        <Image src="/logo.png" alt="logo" width={60} height={60} />
+        <span className="ml-2 text-xl font-bold text-purple-800">
+          ReaderAI Labs
+        </span>
       </Link>
       {/* Navigation Buttons */}
       <div className="mt-2 mr-2 flex flex-wrap gap-3">
